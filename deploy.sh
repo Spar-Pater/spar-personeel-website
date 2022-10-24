547363388bbf306c669c21fb75ddbefd38708427
Dennis Pater <d.pater1986@gmail.com>
2022-10-24 17:22:22 +0200

    FIX deploy set with key

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 #
 
 cd /home/dennis/github/spar-haccp-website
-runuser -l dennis -c 'git pull origin main'
+runuser -l dennis -c 'git pull origin main -key /home/dennis/.ssh/id_ed25519'
 runuser -l dennis -c 'hugo'
 runuser -l dennis -c 'git add .'
 runuser -l dennis -c 'git commit -am "Run script"'
-runuser -l dennis -c 'git push origin main'
+runuser -l dennis -c 'git push origin main -key /home/dennis/.ssh/id_ed25519'
 rsync -avh --delete ./public/ /var/www/html/
 
